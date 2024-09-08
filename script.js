1c832f3b13d4b3f35584c71d0cae7d3617381e60
HoHoInc <noahjones24@cloudflare.gay>
2024-09-08 14:21:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('download-button').addEventListener('click', function() {
+    alert('You are about to download EncryptorX. This is a real of a cheat tool and is provided as-is.');
+});
